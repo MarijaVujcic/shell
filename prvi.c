cd97d74fff1f2893b9d209faea070068ae841297
MarijaVujcic <44531885+MarijaVujcic@users.noreply.github.com>
2019-01-18 11:54:14 +0100

    Add files via upload

--- prvi.c
+++ prvi.c
@@ -11,6 +11,8 @@ int path(char **args);
 int lsh_exit(char **args);
 int lsh_echo(char **args);
 int lst(char **args);
+int echo(char **args);
+int clr(char **args);
 
 char *builtin_str[] = {
   "cd",
@@ -34,9 +36,11 @@ int (*builtin_func[]) (char **) = {
 };
 
 
-int lsh_num_builtins() {
+int lsh_num_builtins() 
+{
   return sizeof(builtin_str) / sizeof(char *);
 }
+
 int lsh_cd(char **args)
 {
   if (args[1] == NULL) {
@@ -51,20 +55,24 @@ int lsh_cd(char **args)
 int lsh_help(char **args)
 {
   int i;
-  printf("Stephen Brennan's LSH\n");
+  printf("MArija VUjcic's LSH\n");
   printf("Type program names and arguments, and hit enter.\n");
   printf("The following are built in:\n");
-  for (i = 0; i < lsh_num_builtins(); i++) {
+  for (i = 0; i < lsh_num_builtins(); i++)
+  {
     printf("  %s\n", builtin_str[i]);
   }
+}
 int lsh_exit(char **args)
 {
         printf("\n\nRegards ,until next time ! :)\n\n");
   return 0;
 }
-int echo(char** args) {
+int echo(char** args) 
+{
   int i = 1;
-  while(args[i] != NULL) {
+  while(args[i] != NULL) 
+  {
     printf("%s ", args[i]);
     i++;
   }
@@ -78,18 +86,25 @@ int lsh_launch(char **args)
   int status;
 
   pid = fork();
-  if (pid == 0) {
+  if (pid == 0) 
+  {
     // Child process
-    if (execvp(args[0], args) == -1) {
+    if (execvp(args[0], args) == -1)
+    {
       perror("lsh");
     }
     exit(EXIT_FAILURE);
-  } else if (pid < 0) {
+  }
+  else if (pid < 0) 
+  {
     // Error forking
     perror("lsh");
-  } else {
+  }
+  else
+  {
     // Parent process
-    do {
+    do
+    {
       waitpid(pid, &status, WUNTRACED);
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
   }
@@ -101,13 +116,16 @@ int lsh_execute(char **args)
 {
   int i;
 
-  if (args[0] == NULL) {
+  if (args[0] == NULL)
+ {
     // An empty command was entered.
     return 1;
   }
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
-    if (strcmp(args[0], builtin_str[i]) == 0) {
+  for (i = 0; i < lsh_num_builtins(); i++)
+  {
+    if (strcmp(args[0], builtin_str[i]) == 0)
+    {
       return (*builtin_func[i])(args);
     }
   }
@@ -152,6 +170,7 @@ int lst(char **args)
 int clr(char **args)
 {
 		system("clear");
+		return 1;
 }	
 void init_shell()
 {
@@ -170,30 +189,39 @@ char *lsh_read_line(void)
   int position = 0;
   char *buffer = malloc(sizeof(char) * bufsize);
   int c;
-  if (!buffer) {
+  if (!buffer)
+  {
     fprintf(stderr, "lsh: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
-  while (1) {
+  while (1)
+  {
     // Read a character
     c = getchar();
 
-    if (c == EOF) {
+    if (c == EOF)
+    {
       exit(EXIT_SUCCESS);
-    } else if (c == '\n') {
+    }
+    else if (c == '\n')
+    {
       buffer[position] = '\0';
       return buffer;
-    } else {
+    }
+    else
+    {
       buffer[position] = c;
     }
     position++;
 
     // If we have exceeded the buffer, reallocate.
-    if (position >= bufsize) {
+    if (position >= bufsize)
+    {
       bufsize += LSH_RL_BUFSIZE;
       buffer = realloc(buffer, bufsize);
-      if (!buffer) {
+      if (!buffer)
+      {
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
       }
@@ -213,29 +241,33 @@ char **lsh_split_line(char *line)
   char **tokens = malloc(bufsize * sizeof(char*));
   char *token, **tokens_backup;
 
-  if (!tokens) {
+  if (!tokens)
+ {
     fprintf(stderr, "lsh: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
   token = strtok(line, LSH_TOK_DELIM);
-  while (token != NULL) {
+  while (token != NULL)
+ {
     tokens[position] = token;
     position++;
 
-    if (position >= bufsize) {
+    if (position >= bufsize)
+   {
       bufsize += LSH_TOK_BUFSIZE;
       tokens_backup = tokens;
       tokens = realloc(tokens, bufsize * sizeof(char*));
-     if (!tokens) {
+     if (!tokens) 
+      {
                 free(tokens_backup);
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
       }
-    }
+   }
 
     token = strtok(NULL, LSH_TOK_DELIM);
-  }
+ }
   tokens[position] = NULL;
   return tokens;
 }
@@ -250,7 +282,8 @@ void lsh_loop(void)
   getcwd(cwd,sizeof(cwd));
   char *username =getenv("USER");
 
-  do {
+  do
+  {
     getcwd(cwd,sizeof(cwd));
     printf("%s shellu-@%s >>",cwd ,username);
     line = lsh_read_line();
@@ -265,7 +298,7 @@ void lsh_loop(void)
 
 int main(int argc, char **argv)
 {
-  clr();
+  system("clear");
   init_shell();
   lsh_loop();
 
